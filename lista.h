34774d253512cbf50c30963f65a75792b07b546f
dougas-vieira <douglasmaximos@gmail.com>
2015-09-15 22:57:39 -0300

    comentando no list.h, oq as funções de inserção fazem.

--- lista.h
+++ lista.h
@@ -9,19 +9,23 @@ palestra *cria_list_palestra (void)
 disponibilidade *cria_list_disponibilidade (void)
 {
 	return NULL;
-	
 }
-
 
 palestrante *insere_list_palestrante (palestrante *ini_list_palestrante,palestrante *fim_list_palestrante)
 {
+	//insere no final
+	//rece um ponteiro pro inicio e pro fim da list_palestrante e insere no final.
 	return ;
 }
 palestra *insere_list_palestra (palestra *ini_list_palestra,palestra *fim_list_palestra)
 {
+	//insere no final
+	//rece um ponteiro pro inicio e pro fim da list_palestra e insere no final.
 	return ;
 }
-disponibilidade *insere_list_disponibilidade (void)
+disponibilidade *insere_list_disponibilidade (palestrante *elemento_palestrante) 
 {
+	// insere no final
+	//recebe ponteiro pro elemento da lista palestrante, que é o inicio da lista de disponibilidade e insere la.
 	return ;
 }
